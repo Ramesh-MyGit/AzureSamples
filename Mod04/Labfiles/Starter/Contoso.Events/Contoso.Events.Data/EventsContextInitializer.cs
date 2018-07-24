b04cc69494a633c09a0538824bc55dca34548690
Ramesh <ramesh83.cbe@gmail.com>
2018-07-24 15:29:04 +0000

    first commit

---
+++ Mod04/Labfiles/Starter/Contoso.Events/Contoso.Events.Data/EventsContextInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Contoso.Events.Models;
+
+namespace Contoso.Events.Data
+{
+    public class EventsContextInitializer: CreateDatabaseIfNotExists<EventsContext>
+    {
+        protected override void Seed(EventsContext context)
+        {
+            if (context.Events.Count() == 0) {
+                Event eventItem = new Event();
+                eventItem.EventKey = "FY17SepGeneralConference";
+                eventItem.StartTime = DateTime.Today;
+                eventItem.EndTime = DateTime.Today.AddDays(3d);
+                eventItem.Title = "FY17 September Technical Conference";
+                eventItem.Description = "Sed in euismod mi.";
+                eventItem.RegistrationCount = 1;
+                context.Events.Add(eventItem);
+            }
+            if (context.Registrations.Count() == 0) {
+                Registration registrationItem = new Registration();
+                registrationItem.EventKey = "FY17SepGeneralConference";
+                registrationItem.FirstName = "Aisha";
+                registrationItem.LastName = "Witt";
+                context.Registrations.Add(registrationItem);
+            }
+            context.SaveChanges();
+        }
+    }
+}
